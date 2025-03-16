555c78320925a6e4af2d15a9a46d096a99f0944d
6604062630226 <152473809+6604062630226@users.noreply.github.com>
2025-03-16 19:06:29 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("📌 Hello")
+st.write("เลือกหน้าต่างๆได้จาก Sidebar")
+st.write("จัดทำโดย นายธนธรณ์ เรืองต่อวงศ์ (6604062630226)")
+st.write("มีทั้งหมด 4 หน้า\n"
+"- Machine Learning Introduction(ml intro)\n"
+"- Machine Learning Demo(ml demo)\n"
+"- Neural Network Introduction(nn intro)\n"
+"- Neural Network Demo(nn demo)")
